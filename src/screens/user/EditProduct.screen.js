d55bd369bd52b291e9dfda87ce3b16e66e8b862d
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-12-11 11:36:55 -0400

    cleaning up some unused code and starting the form to edit and add new products

--- src/screens/user/EditProduct.screen.js
+++ src/screens/user/EditProduct.screen.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { AntDesign } from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const EditProduct = ({ navigation }) => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Image URL</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Price</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput style={styles.input} />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -36,5 +54,22 @@ const styles = StyleSheet.create({
   headerTitle: {
     fontFamily: 'poppins-regular',
     fontSize: 18,
+  },
+  form: {
+    margin: 20,
+  },
+  formControl: {
+    paddingVertical: 15,
+    width: '100%',
+  },
+  label: {
+    fontFamily: 'poppins-regular',
+    marginVertical: 8,
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
 });
